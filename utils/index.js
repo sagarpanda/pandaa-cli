be4572916874b8d7fc1ce5b39585ea76869f76e8
Sagar Panda <er.sagarpanda@gmail.com>
2020-05-21 20:55:55 +0530

    updated utils

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const Mustache = require('mustache');
+
+const excludeList = ['.git', '.next', '.DS_Store', 'yarn.lock'];
 
 module.exports = {
   getCurrentDirectoryBase: () => {
@@ -8,5 +11,37 @@ module.exports = {
 
   directoryExists: (filePath) => {
     return fs.existsSync(filePath);
+  },
+
+  getAllFiles: (srcDir, level = 0) => {
+    let files = [];
+    if(level < 0) { return [] }
+    fs.readdirSync(srcDir).map(name => {
+      const fullpath = `${srcDir}/${name}`;
+      const isDir = fs.statSync(fullpath).isDirectory();
+      if(!excludeList.includes(name)) {
+        if(isDir) {
+          const temp = getAllFiles(fullpath, level - 1);
+          files = files.concat(temp);
+        } else {
+          files.push(fullpath);
+        }
+      }
+    });
+    return files;
+  },
+
+  generateFiles: () => {
+    // console.log(process.cwd(), __dirname)
+    const files = getAllFiles('/Users/sagarpanda/Projects/Misc/snoopy', 10);
+    files.forEach(item => {
+      const tpl = fs.readFileSync(
+        '/Users/sagarpanda/Projects/Misc/ucinema/a.js',
+        { encoding:'utf8', flag:'r' }
+      ); 
+      const rendered = Mustache.render(tpl, { name: 'Luke1' });
+      console.log(rendered);
+      //fs.writeFileSync("/Users/sagarpanda/Projects/Misc/ucinema/programming.txt", xxx); 
+    });
   }
 };
